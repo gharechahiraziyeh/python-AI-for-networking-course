af67fee52e40a9d37e1728781c19e32e4d27272a
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-11-25 19:21:40 +0330

    sbp run and save output in file and type output to string

--- 3-session/3-sbp-run2.py
+++ 3-session/3-sbp-run2.py
@@ -4,4 +4,4 @@ output = sbp.run(["ping", "4.2.2.4"], capture_output=True)
 #print(f" the output type is {type(output)}")
 print(output)
 with open("output.txt", "w") as file:
-    result = sbp.run(["ping", "4.2.2.4"], stdout=file)
+    result = sbp.run(["ping", "4.2.2.4"], stdout=file, text=True)
