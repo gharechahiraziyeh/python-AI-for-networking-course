28448671b14408a2233019979141d5e7656ee5a3
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-11-25 18:53:34 +0330

    sbp run without print and  don't show output

--- 3-session/2- sbp-run.py
+++ 3-session/2- sbp-run.py
@@ -1,6 +1,6 @@
 import subprocess as sbp
 
 ip = "4.2.2.4"
-output = sbp.run(["ping", "4.2.2.4"])
-print(f" the output type is {type(output)}")
-print(output)
+output = sbp.run(["ping", "4.2.2.4"], capture_output=True)
+#print(f" the output type is {type(output)}")
+#print(output)
