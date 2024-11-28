ac45e2014f57e56d37e8dae05d41375ff1c96fa1
raziyeh gharechahi <r.gharechahi_64@yahoo.com>
2024-11-28 11:34:12 +0330

    ssh to R1 with ssh to three router

--- 2-session/7-ssh to three router.py
+++ 2-session/7-ssh to three router.py
@@ -11,7 +11,15 @@ device1 = {
 
 device2 = {
     "device_type": "cisco_ios",
-    "host": "192.168.175.2",
+    "host": "192.168.175.3",
+    "username": "root",
+    "password": "1234",
+    "port": "22"
+}
+
+device3 = {
+    "device_type": "cisco_ios",
+    "host": "192.168.175.4",
     "username": "root",
     "password": "1234",
     "port": "22"
@@ -22,7 +30,7 @@ device2 = {
 
 command = ["interface loopback 1", "ip add 1.1.1.1 255.0.0.0"]
 
-device_list = [device1, device2]
+device_list = [device1, device2, device3]
 
 for device in device_list:
     with ConnectHandler(**device) as ssh:
